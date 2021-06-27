f9557c9d6a37a5b140afe6b183f11bea3e326755
Rhishna Capital <85033473+naisayuriza@users.noreply.github.com>
2021-06-27 20:05:25 +0700

    Add files via upload

---
+++ message/help.js
@@ -0,0 +1,106 @@
+exports.menu = (prefix, i) => {
+    return `┏━━❬ *INFORMATION* ❬━━━━━━━❭
+┣➥• Nama Bot : CyberBotz
+┣➥• Prefix Bot : [ ${prefix} ]
+┏━━❬ *RULES BOT* ❬━━━━━━━❭
+┣➥• TELPON/VC [BAN]
+┣➥• SPAM [BLOCK]
+┗━━━━━━━━━━━━━━━━━━❭
+┌──〘 INFO 〙
+│⦿ ${i.free}  ${prefix}stats
+│⦿ ${i.free}  ${prefix}limit
+│⦿ ${i.free}  ${prefix}balance
+│⦿ ${i.free}  ${prefix}runtime
+│⦿ ${i.free}  ${prefix}speed
+│⦿ ${i.free}  ${prefix}donasi
+│⦿ ${i.free}  ${prefix}tutorial
+│⦿ ${i.free}  ${prefix}sewabot
+│⦿ ${i.free}  ${prefix}simplemenu
+│⦿ ${i.free}  ${prefix}owner
+└────────────────────〙
+┌──〘 TOOLS 〙
+│⦿ ${i.limit} ${prefix}sticker
+│⦿ ${i.prem} ${prefix}swm
+│⦿ ${i.prem} ${prefix}take
+│⦿ ${i.limit} ${prefix}toimg
+│⦿ ${i.limit} ${prefix}attp text
+│⦿ ${i.limit} ${prefix}tinyurl url
+│⦿ ${i.limit} ${prefix}nuliskiri text
+│⦿ ${i.limit} ${prefix}nuliskanan text
+│⦿ ${i.limit} ${prefix}foliokiri text
+│⦿ ${i.limit} ${prefix}foliokanan text
+│⦿ ${i.limit} ${prefix}translate kodebahasa [reply pesan/text]
+└────────────────────〙
+┌──〘 DOWNLOAD 〙
+│⦿ ${i.limit} ${prefix}ytmp4 link
+│⦿ ${i.limit} ${prefix}ytmp3 link
+│⦿ ${i.limit} ${prefix}igdl link
+│⦿ ${i.limit} ${prefix}fbdl link
+│⦿ ${i.limit} ${prefix}yts judul
+│⦿ ${i.limit} ${prefix}play judul
+│⦿ ${i.limit} ${prefix}playmp4 judul 
+└────────────────────〙
+┌──〘 STALKER 〙
+│⦿ ${i.limit} ${prefix}igstalk username
+│⦿ ${i.limit} ${prefix}ghstalk username
+└────────────────────〙
+┌──〘 MAKER 〙
+│⦿ ${i.limit} ${prefix}blackpink text
+│⦿ ${i.limit} ${prefix}tahta text
+│⦿ ${i.limit} ${prefix}neon text
+│⦿ ${i.limit} ${prefix}glitch text1|text2
+└────────────────────〙
+┌──〘 BAILEYS 〙
+│⦿ ${i.free} ${prefix}tagme
+│⦿ ${i.limit} ${prefix}kontak nomor|nama
+│⦿ ${i.prem} ${prefix}hidetag
+└────────────────────〙
+┌──〘 PREMIUM 〙
+│⦿ ${i.owner} ${prefix}addprem @tag
+│⦿ ${i.owner} ${prefix}delprem @tag
+│⦿ ${i.owner} ${prefix}cekprem
+│⦿ ${i.free} ${prefix}listprem
+└────────────────────〙
+┌──〘 GAME 〙
+│⦿ ${i.glimit} ${prefix}tictactoe @tag
+│⦿ ${i.glimit} ${prefix}tebakgambar
+│⦿ ${i.glimit} ${prefix}family100
+└────────────────────〙
+┌──〘 GRUP 〙
+│⦿ ${i.owner} ${prefix}welcome 1/0
+│⦿ ${i.free} ${prefix}afk
+│⦿ ${i.free} ${prefix}infogrup
+│⦿ ${i.free} ${prefix}chatinfo
+│⦿ ${i.owner} ${prefix}add 628xx
+│⦿ ${i.owner} ${prefix}kick @tag
+│⦿ ${i.owner} ${prefix}promote @tag
+│⦿ ${i.owner} ${prefix}demote @tag
+│⦿ ${i.free} ${prefix}linkgc
+│⦿ ${i.owner} ${prefix}leave
+│⦿ ${i.owner} ${prefix}setdesc
+│⦿ ${i.owner} ${prefix}setgrupname
+│⦿ ${i.owner} ${prefix}setppgrup
+│⦿ ${i.owner} ${prefix}opengrup
+│⦿ ${i.owner} ${prefix}closegrup
+│⦿ ${i.owner} ${prefix}join
+│⦿ ${i.owner} ${prefix}tagall
+│⦿ ${i.owner} ${prefix}mute
+│⦿ ${i.owner} ${prefix}unmute
+└────────────────────〙
+┌──〘 OWNER 〙
+│⦿ ${i.owner} ${prefix}ban @tag
+│⦿ ${i.owner} ${prefix}unban @tag
+│⦿ ${i.free} ${prefix}listban
+│⦿ ${i.owner} >
+│⦿ ${i.owner} $
+│⦿ ${i.owner} ${prefix}setprefix
+│⦿ ${i.owner} ${prefix}bc
+│⦿ ${i.owner} ${prefix}exif nama|author
+└────────────────────〙
+┌> Note :
+│⦿ ${i.owner} : Fitur Admin Grup atau Owner
+│⦿ ${i.free} : Fitur gratis yang tidak memakai limit
+│⦿ ${i.limit} : Fitur yang memakai 1 limit harian
+│⦿ ${i.glimit} : Fitur yang memakai 1 limit game
+│⦿ ${i.prem} : Fitur khusus premium user`
+}
